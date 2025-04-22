f56a72ba5ecd95b15b4151dd19ddd9febc60b2e2
Bordocklius <miguel.dhaenens@hotmail.com>
2025-04-22 18:58:02 +0200

    Work started on character controls

---
+++ Assets/Scripts/PlayerCharacterControler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerCharacterControler : MonoBehaviour
+{
+    public PlayerControls _inputActions;
+    public CharacterController _characterController;
+    
+    public float _speed;
+
+    private Vector2 _movementDirection;
+
+    void Awake()
+    {
+        _inputActions = new PlayerControls();
+
+        _inputActions.Gameplay.Move.performed += ctx => _movementDirection = ctx.ReadValue<Vector2>();
+        _inputActions.Gameplay.Move.canceled += ctx => _movementDirection = Vector2.zero;
+
+        _inputActions.Gameplay.Jump.performed += ctx => Jump();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Gameplay.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_movementDirection);
+        if(_movementDirection != Vector2.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 movement = new Vector3(_movementDirection.x, 0, _movementDirection.y);
+        movement *= _speed * Time.deltaTime;
+        _characterController.Move(movement);
+    }
+
+    private void Jump()
+    {
+        
+    }
+}
